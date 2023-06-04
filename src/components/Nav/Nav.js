77cc61b3a8d584fa96a88451769dfbb2e71401a1
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2023-06-04 12:12:55 +0200

    dropdown user nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,18 @@
 import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
 import '../Nav/Nav.scss'
 import logo from '../../assets/logo.svg'
+import { USER_MAIN_DATA } from '../../data/Data';
 
 function Nav() {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleClick = () => {
+        setIsVisible(!isVisible);
+    };
+
+  const dropdown = isVisible ? 'dropdown visible' : 'dropdown';
 
     return (
         <nav className="nav"> 
@@ -10,7 +20,17 @@ function Nav() {
                 <img src={logo} className="nav_logo" alt='Logo SportSee'/>
             </Link> 
             <Link to="/" className="nav_link">Accueil</Link>
-            <Link to="/" className="nav_link">Profil</Link>
+            <div className="nav_link" onClick={handleClick}>Profil
+                <div className="nav_link-container">
+                    <ul className={dropdown}>
+                        {USER_MAIN_DATA.map(({id, userInfos}) =>           
+                            <Link to={`/user/${id}`} key={id} className="dropdown_user">
+                                {`${userInfos.firstName} ${userInfos.lastName}`}
+                            </Link>   
+                        )}    
+                    </ul>
+                </div>            
+            </div>
             <Link to="/" className="nav_link">Réglages</Link>
             <Link to="/" className="nav_link">Communauté</Link>
         </nav>
