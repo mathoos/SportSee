1543d2a2af67027e68dc5a539af3af776dd5208b
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2023-07-26 08:27:59 +0200

    responsive 1024x780

--- src/components/PieChart/PieChart.js
+++ src/components/PieChart/PieChart.js
@@ -13,18 +13,18 @@ function PieChartComponent({ user }) {
 
   
     return (
-        <div className="pie-container">    
-            <p className="pie-container_score">{percentage}% <br/>de votre objectif</p>
-    
-            <ResponsiveContainer className="responsive-container">
+        <div className="pie-container">   
+            <p className="pie-container_title">Score</p> 
+            <p className="pie-container_score"><span>{percentage}%</span> <br/>de votre <br/> objectif</p> 
+            <ResponsiveContainer>
                 <PieChart>
-                    <Pie nameKey="name"cx="50%" cy="50%" startAngle={-270} stroke="none" data={data} dataKey="value" outerRadius={85} innerRadius={75} paddingAngle={0} cornerRadius={50}>
+                    <Pie nameKey="name"cx="50%" cy="50%" startAngle={-270} stroke="none" data={data} dataKey="value" outerRadius={90} innerRadius={80} paddingAngle={0} cornerRadius={50}>
                         {data.map((entry, index) => (
                             <Cell key={`cell-${index}`} className={`${entry.name === 'Difference' ? 'difference-path' : ''} ${entry.name === 'Percentage' ? 'percentage-path' : ''}`}/>
                         ))}
                     </Pie>
                     <Pie nameKey="name" cx="50%" cy="50%" startAngle={-270} endAngle={90} stroke="none" dataKey="value" 
-                        outerRadius={75} innerRadius={0} className="circle-path"
+                        outerRadius={80} innerRadius={0} className="circle-path"
                         data={[{ name: 'Full', value: 100 }]} >
                     </Pie>
                 </PieChart>
--- src/components/RadarChart/RadarChart.js
+++ src/components/RadarChart/RadarChart.js
@@ -6,7 +6,7 @@ function RadarChartComponent({ radarData }) {
     return (
         <div className="radar-container">
             <ResponsiveContainer>
-                <RadarChart className="radar" cx="50%" cy="50%" outerRadius="60%" data={radarData}>
+                <RadarChart className="radarChart" cx="50%" cy="50%" outerRadius="60%" data={radarData}>
                 <PolarGrid gridType="polygon" radialLines={false} />
                     <PolarAngleAxis dataKey="kind" tick={{ fontSize: 12, fill: 'rgba(255, 255, 255, 1)' }}/>
                     <Radar name="Performance" dataKey="value" fill="rgba(255, 1, 1, 0.7)" ickOffset={100}/>
